14e227f784ff0c55c36b50633db4e4ba876c6ba4
danielts90 <danieltadeu2.dt@gmail.com>
2018-02-09 15:23:54 -0200

    Restaurantes dinamicos

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -10,12 +10,14 @@ import { Oferta } from '../shared/oferta.model';
 })
 export class RestaurantesComponent implements OnInit {
 
+  public ofertas: Oferta[];
+
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
     this.ofertasService.getOfertasPorCategoria('restaurante')
     .then(( ofertas: Oferta[]) => {
-      console.log(ofertas);
+      this.ofertas = ofertas;
     });
   }
 
