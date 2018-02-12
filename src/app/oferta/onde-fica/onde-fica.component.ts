263a05290f241985939fbaabca43ca88cdba80e8
danielts90 <danieltadeu2.dt@gmail.com>
2018-02-11 22:37:54 -0200

    Inclusão do OndeFica na Api e no component.

--- src/app/oferta/onde-fica/onde-fica.component.ts
+++ src/app/oferta/onde-fica/onde-fica.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from '../../ofertas.service';
 
 @Component({
   selector: 'app-onde-fica',
   templateUrl: './onde-fica.component.html',
-  styleUrls: ['./onde-fica.component.css']
+  styleUrls: ['./onde-fica.component.css'],
+  providers: [ OfertasService ]
 })
 export class OndeFicaComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  public ondeFica: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    console.log(this.route.parent.snapshot.params['id']);
+     this.ofertasService.getOndeFicaOfertaPorId(this.route.parent.snapshot.params['id'])
+     .then((resposta: string) => {
+       this.ondeFica = resposta;
+     } );
   }
 
 }
