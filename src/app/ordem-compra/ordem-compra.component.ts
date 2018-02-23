591892af019879c56735c3ea2e29b8f672af1f92
danielts90 <danieltadeu2.dt@gmail.com>
2018-02-22 23:14:53 -0300

    Envio de dados do formulário via Post

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -85,7 +85,9 @@ export class OrdemCompraComponent implements OnInit {
     this.pedido.numero = this.numero;
     this.pedido.complemento = this.complemento;
     this.pedido.formaPagamento = this.formaPagamento;
-    this.ordemCompraService.efetivarCompra(this.pedido);
+
+    this.ordemCompraService.efetivarCompra(this.pedido)
+      .subscribe();
   }
 
 
